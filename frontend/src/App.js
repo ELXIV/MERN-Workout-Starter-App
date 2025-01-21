a4c0b82bd68eb43a99f2b9ab0af4ba6eb849d659
Elyza Gaile <elyzagailefabm@gmail.com>
2025-01-21 15:35:11 +0800

    yes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
